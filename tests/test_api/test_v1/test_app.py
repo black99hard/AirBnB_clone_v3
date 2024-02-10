c97f22cb6ae4f70e563b48289f1b4accffe9f6f3
black99hard <yasser99hard@gmail.com>
2024-02-10 10:57:36 +0100

    d

--- tests/test_api/test_v1/test_app.py
+++ tests/test_api/test_v1/test_app.py
@@ -1,45 +1,28 @@
 #!/usr/bin/python3
 """
-Unit Test for api v1 Flask App
+Testing app.py file
 """
-from api.v1 import app
-import inspect
-import pep8
+from api.v1.app import app
+import flask
+import json
+from models import storage
 import unittest
 
 
-class TestAppDocs(unittest.TestCase):
-    """Class for testing Flask App docs"""
-
-    all_funcs = inspect.getmembers(app, inspect.isfunction)
+class TestApp(unittest.TestCase):
 
     @classmethod
-    def setUpClass(cls):
-        print('\n\n.................................')
-        print('..... Testing Documentation .....')
-        print('........  For Flask App  ........')
-        print('.................................\n\n')
+    def setUpClass(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
 
-    def test_doc_file(self):
-        """... documentation for the file"""
-        actual = app.__doc__
-        self.assertIsNotNone(actual)
-
-    def test_all_function_docs(self):
-        """... tests for ALL DOCS for all functions in db_storage file"""
-        all_functions = TestAppDocs.all_funcs
-        for function in all_functions:
-            self.assertIsNotNone(function[1].__doc__)
-
-    def test_pep8_app(self):
-        """... app.py conforms to PEP8 Style"""
-        pep8style = pep8.StyleGuide(quiet=True)
-        errors = pep8style.check_files(['api/v1/app.py'])
-        self.assertEqual(errors.total_errors, 0, errors.messages)
+    def test_404(self):
+        rv = self.app.get('/bad')
+        self.assertEqual(rv.status_code, 404)
+        self.assertEqual(rv.headers.get("Content-Type"), "application/json")
+        json_format = json.loads(str(rv.get_data(), encoding="utf-8"))
+        self.assertEqual(json_format.get("error"), "Not found")
 
 
-if __name__ == '__main__':
-    """
-    MAIN TESTS
-    """
-    unittest.main
+if __name__ == "__main__":
+    unittest.main()
