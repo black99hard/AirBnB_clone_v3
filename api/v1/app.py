1ce1499fed6b859f46e610a38692f57290c40ddb
black99hard <yasser99hard@gmail.com>
2024-02-10 11:23:12 +0100

    new api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
-"""
-Module app
-"""
-from api.v1.views import app_views
-from flasgger import Swagger
-from flask import (Blueprint, Flask, jsonify, make_response)
-from flask_cors import (CORS, cross_origin)
+"""app.py to connect to API"""
+import os
 from models import storage
-from os import getenv
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
-Swagger(app)
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """json 404 page"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """ closes the session """
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", "5000")
-#    print(app.url_map)
-    app.run(host=host, port=port)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
